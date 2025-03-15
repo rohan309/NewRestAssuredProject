fbacc6786805ccadce0d8efb24e67b25d7d1824a
rohan309 <rohanmore309@gmail.com>
2025-03-15 23:22:52 +0530

    pom.xml updated and TestRunner is added

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,3 +1,16 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/com.api_automation/Features",
+        glue = "com.api_automation/stepdefination"
+        /*plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "junit:target/cucumber-reports/Cucumber.xml"
+        }*/
+)
 public class TestRunner {
 }
